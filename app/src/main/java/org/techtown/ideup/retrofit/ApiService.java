f9bf77158cfe1ba7a2f1fdbc7eefbd83d9232894
ayoung0073 <ayong0310@naver.com>
2021-02-11 21:19:54 +0900

    Fix : code 수정

--- app/src/main/java/org/techtown/ideup/retrofit/ApiService.java
+++ app/src/main/java/org/techtown/ideup/retrofit/ApiService.java
@@ -10,13 +10,6 @@ import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 public interface ApiService {
-//    @GET("/diarys") // 챌린지목록(추천챌린지포함)
-//    Call<ArrayList<Diary>> getDiarys();
-//    @GET("/") // 챌린지목록(추천챌린지포함)
-//    Call<String> getTest();
-//    @POST("/diarys/") // 사진업로드
-//    Call<Diary> postDiary(@Body Diary diary);
-
     @GET("/projects")
     Call<ArrayList<ProjectDto>> getProjectList(); // 프로젝트 리스트 받아오기
 }
